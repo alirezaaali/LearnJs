d8f96486d32c0e74a2a6cf78e5316da6fafa7ef1
Alireza <alireza.aali@gmail.com>
2020-03-17 23:14:48 +0330

    if else statement

--- externalJSFile.js
+++ externalJSFile.js
@@ -1,7 +1,21 @@
-//Loop Sample
-var age = 20;      
+/*Loop Sample
+var age = 15;      
 if( age > 18 ) {
     document.write("<b>Qualifies for driving</b>");
 }
-
+else
+{
+    document.write("<b>NOT Qualifies for driving</b>");
+}
+*/
+var book = "maths";
+if( book == "history" ) {
+   document.write("<b>History Book</b>");
+} else if( book == "maths" ) {
+   document.write("<b>Maths Book</b>");
+} else if( book == "economics" ) {
+   document.write("<b>Economics Book</b>");
+} else {
+   document.write("<b>Unknown Book</b>");
+}
 

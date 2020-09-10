b0640d44b7c165ed9bee7d9b526e7e463ce399c6
AlirezaAali <alireza.aali@gmail.com>
2020-09-10 12:57:48 +0430

    session 45 is finishid - Heads and Tails is ready to Play

--- TopLearn_Js/JavaScripts/myJsCodes.js
+++ TopLearn_Js/JavaScripts/myJsCodes.js
@@ -304,7 +304,7 @@ request.onreadystatechange = function () {
     }
 }
 request.send();
-*/
+
 
 //--------------------------------------------------------------
 //index41
@@ -360,4 +360,42 @@ function showfull() {
     }
     request.send();
 
+}
+*/
+
+//--------------------------------------------------------------
+//index43
+//debugger
+const CoinArray = ["Heads", "Tails"];
+const message = document.querySelector(".message");
+const buttons = document.querySelectorAll("button");
+let Score = [0, 0];
+let Output = "";
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", tossCoin);
+}
+
+function tossCoin(e) {
+  let PlayerGuss = e.target.innerText;
+  let ComputerToss = Math.floor(Math.random() * 2);
+  let ComputerGuss = CoinArray[ComputerToss];
+
+  message.innerHTML = "Computer Selected  ===>  " + ComputerGuss;
+  if (PlayerGuss === ComputerGuss) {
+    Output = " || You Win! ";
+    Score[0]++;
+    message.style.color = "blue";
+  } else {
+    Output = " || You lose! ";
+    message.style.color = "red";
+    Score[1]++;
+  }
+
+  message.innerHTML +=
+    Output +
+    "<br /> Player Score: " +
+    Score[0] +
+    " || Computer Score:" +
+    Score[1];
 }
